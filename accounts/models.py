0694879ae261c6ff424277cb78cc95ea70a5233c
thalals <thalsal@naver.com>
2020-09-05 17:36:39 +0900

    [add] user models

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,25 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+class User(AbstractUser)
+
+    """ Custom user Model """
+    GENDER_MALE = "male"
+    GENDER_FEMALE = "female"
+    
+    # tuple
+    GENDER_CHOICES = (
+        (GENDER_MALE, "Male"),
+        (GENDER_FEMALE, "Female"),
+    )   
+
+    avatar = models.ImageField(blank=True)  # 프로필
+    gender = models.CharField(
+        choices=GENDER_CHOICES,
+        max_length=10,
+        blank=True,
+        default=GENDER_MALE,  # choices 의 default 값
+    )
+
+    age = models.IntegerField()
