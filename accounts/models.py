f75ad0b247effc1d61724156501a1a3bcd99ce83
thalals <thalsal@naver.com>
2020-09-06 00:25:42 +0900

    .

--- accounts/models.py
+++ accounts/models.py
@@ -15,12 +15,12 @@ class User(AbstractUser):
         (GENDER_FEMALE, "Female"),
     )
 
-    avatar = models.ImageField(blank=True)  # 프로필
-    gender = models.CharField(
+    avatar = models.ImageField(blank=True)  # 프로필사진
+    gender = models.CharField(  # 성별
         choices=GENDER_CHOICES,
         max_length=10,
         blank=True,
         default=GENDER_MALE,  # choices 의 default 값
     )
 
-    age = models.IntegerField(null=True)
+    age = models.IntegerField(null=True)  # 나이
