c1e869c95791571d508a84648114e4ba64bda9d1
minsgy <alstjrchl97@naver.com>
2020-09-05 23:20:30 +0900

    [comment] model 추가 [orders] model 변경

--- orders/models.py
+++ orders/models.py
@@ -5,10 +5,8 @@ class Order(models.Model):
 
     """ Order models """
 
-    book = models.ForeignKey("essays.Essays", on_delete=models.CASCADE)
-    user_name = models.CharField(max_length=50)
-
-    adress = models.CharField(max_length=150)
-    book_count = models.IntegerField()
-
-    price = models.IntegerField()
+    book = models.ForeignKey("essays.Essays", on_delete=models.CASCADE) # 주문 할 책 가져오기
+    user_name = models.CharField(max_length=50) # 주문자 이름
+    address = models.CharField(max_length=150) # 주문자 주소
+    book_count = models.IntegerField() # 책 구매 개수
+    price = models.IntegerField() # 가격
