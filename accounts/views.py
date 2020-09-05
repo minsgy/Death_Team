f6c633cef65f470447a927695ca1de05b61caeed
minsgy <alstjrchl97@naver.com>
2020-09-06 00:54:29 +0900

    [Config] urls - account

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from . import User
+from django.contrib import auth
 # Create your views here.
+
+def login(request):
+    pass
+
+def logout(request):
+    pass
+
+def signup(request):
+    if request.method == "POST":
+        if request.POST["password1"] == request.POST["password2"]:
+            user = User.objects.create_user(username=request.POST["username"], password=request.POST["password1"])
+            # 아이디, 
+            auth.login(request, user)
+            return redirect('index') # 그룹 선택 화면
+        return render(request, 'signup.html') # 비밀번호 확인 실패 시 다시 돌아감.
+
+    return render(request, 'signup.html') # POST 형식 아닐 시
+
+    
--- config/urls.py
+++ config/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path("/",include("accounts.urls")), 
+    # path("",include("accounts.urls")), 
     # path("/",include("essays.urls")),
     # path("/",include("groups.urls")),
     # path("/",include("lists.urls")),
