12060a73e4a4b870684733e45b226cb0a4a9ccd6
minsgy <alstjrchl97@naver.com>
2020-09-05 22:06:26 +0900

    [Lists] Model

--- essays/models.py
+++ essays/models.py
@@ -5,6 +5,6 @@ from core import models as core_models
 class Essays(core_models.TimeStempedModel):
 
     name = models.CharField(max_length=50)
-    start_page = models.ForeignKey("lists.List")
+   # start_page = models.ForeignKey("lists.List", on_delete=models.CASCADE)
+   # last_page = models.ForeignKey("lists.List", on_delete=models.CASCADE)
 
-    last_page = models.ForeignKey("list.List")
--- lists/models.py
+++ lists/models.py
@@ -4,6 +4,8 @@ from core import models as core_models
 # Create your models here.
 
 
-class List(core_models.TimeStempedModel):
-    now = models.DateTimeField(auto_now_add=True)
-
+class List(core_models.TimeStempedModel): # 일기 
+    now = models.DateTimeField(auto_now_add=True) # 소리
+#    weather = models.ImageField()
+    content = models.TextField(max_length=200)
+#    comment = 
