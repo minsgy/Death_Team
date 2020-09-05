4f17a63a3702f02f9222135651c8ac2e96641482
minsgy <alstjrchl97@naver.com>
2020-09-06 00:04:43 +0900

    [Lists]외래키 중복해결

--- lists/models.py
+++ lists/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 from core import models as core_models
-from comment import models as comment_models
-
 # Create your models here.
 
 
@@ -9,8 +7,5 @@ class List(core_models.TimeStempedModel):  # 일기
     now = models.DateTimeField(auto_now_add=True)  # 일기 날짜
     #    weather = models.ImageField() # 일기 날씨
     content = models.TextField(default="", blank=True)  # 일기 내용
-    comment = models.ForeignKey(
-        comment_models.Comment, related_name="list", on_delete=models.CASCADE
-    )  # 댓글
     photos = models.ImageField(upload_to="list_photos")  # 사진
 
