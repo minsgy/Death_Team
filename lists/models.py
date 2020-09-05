8b2378826684cc8b401d9cd267faf0c864909d51
minsgy <alstjrchl97@naver.com>
2020-09-05 18:22:38 +0900

    [Setting] 타임존 세팅, [Lists] 모덷ㄹ

--- lists/models.py
+++ lists/models.py
@@ -1,3 +1,7 @@
 from django.db import models
-
+from core import models as core_models
 # Create your models here.
+
+class List(core_models):
+    now = models.DateTimeField(auto_now_add=True)
+    
