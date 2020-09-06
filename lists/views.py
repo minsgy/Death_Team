bc5590c6e5590acf8cc95a43b4614f8955bddc38
thalals <thalsal@naver.com>
2020-09-06 17:56:54 +0900

    list 에 css 적용

--- lists/views.py
+++ lists/views.py
@@ -7,16 +7,17 @@ def detail(request):
 
     """ list views 일기장 """
 
-    page = List.objects.all() # 모든 리스트 객체 가져오기
+    page = List.objects.all()  # 모든 리스트 객체 가져오기
     page.now = timezone.now()
-    page.content = "불라불라"
+
     return render(request, "list/list.html", {"page": page})
 
 
+# 일기 작성
 def create(request):
-
+    item = List()
     if request.method == "POST":
-        item = models.List()
+        item = List()
 
         item.name = request.POST["title"]  # 제목
         # item.user = request.user  # 작성자
@@ -26,5 +27,6 @@ def create(request):
         item.save()
 
         return redirect("detail")
-    return render(request, "create.html", {"item": item})
+
+    return render(request, "list/create.html", {"item": item})
 
