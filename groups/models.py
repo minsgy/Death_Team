252cf1d5ae1c070ae75e0e4724d6b962ed288195
minsgy <alstjrchl97@naver.com>
2020-09-06 21:36:20 +0900

    다 굥체

--- groups/models.py
+++ groups/models.py
@@ -10,7 +10,7 @@ class Group(core_models.TimeStempedModel):
     name = models.CharField(max_length=150) # 그룹 이름
     uni_code = models.IntegerField(null=True) # 그룹 고유 값 입력받기 
     image = models.ImageField(upload_to='images/') # 책 표지 정하기
-    members = models.ManyToManyField("accounts.User", related_name="group", null=True, blank=True) # User 객체
+    members = models.ManyToManyField("accounts.User", related_name="group", blank=True) # User 객체
 
     def __str__(self):
         return self.name
--- groups/views.py
+++ groups/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from accounts.models import User 
 from .models import Group 
 # Create your views here.
@@ -8,15 +8,12 @@ def group_home(request):
     return render(request, "groups/main.html",{'groups':groups})
 
 def group_create(request):
+    group = Group()
     if request.method == 'POST':
-        group = Group() #객체생성
-        if 'image' in request.FILES: 
-            group.image = request.FILES['image'] # new.html의 name="image"
-        group.name = request.POST['name'] # new.html의 name="name"
+        group.image = request.FILES.get('image') # new.html의 name="image"
+        group.name = request.POST.get('name') # new.html의 name="name"
         group.save()
-        # redirect 함수는 import 해주어야 사용 가능함.
-        # redirect('urls의 path name = 'profile/'+str(post.id), pk)
-        # save 한 후 , detail의 주소로 보내는 것. ( pk(post-id)을 통해 감)
-        # redirect 는 이동만 하는 거고, render 는 새로 만드는 것.
+        print("생성완료")
         return redirect('group_main')
+    print("생성실패") 
     return render(request, 'groups/makegroup.html')
