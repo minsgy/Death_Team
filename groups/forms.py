23476829202815dfe71e3dd11e787890627e37df
minsgy <alstjrchl97@naver.com>
2020-09-06 21:12:14 +0900

    forms 폐기

--- groups/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from .models import Group
-
-class GrForm(forms.ModelForm):
-
-    class Meta:
-        model = Group # 어떤 모델과 대응
-        fields = ('name','image','members')
-
